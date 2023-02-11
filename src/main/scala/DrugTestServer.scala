dc537a1160d4e09b932c837e265525e420304d22
henrybirch <hab.b@hotmail.co.uk>
2023-02-11 16:34:12 +0000

    add docstrings

--- src/main/scala/DrugTestServer.scala
+++ src/main/scala/DrugTestServer.scala
@@ -1,35 +1,48 @@
+import cats.data.Kleisli
 import cats.effect.{ExitCode, IO, IOApp}
 import com.comcast.ip4s.IpLiteralSyntax
 import drugscraper.DrugTestScraper
+import io.circe.Json
 import io.circe.generic.auto._
 import io.circe.syntax.EncoderOps
-import org.http4s.HttpRoutes
 import org.http4s.circe._
 import org.http4s.dsl.Http4sDsl
 import org.http4s.ember.server.EmberServerBuilder
-import org.http4s.server.Router
+import org.http4s.server.{Router, Server}
+import org.http4s.{HttpRoutes, Request, Response}
 
 import java.net.URLDecoder
 
 object DrugTestServer extends IOApp {
   override def run(args: List[String]): IO[ExitCode] = {
-    val app = Router("/" -> drugRoutes).orNotFound
-    EmberServerBuilder.default[IO].withHost(ipv4"0.0.0.0").withPort(port"8080").withHttpApp(app).build.use(_ => IO.never).as(ExitCode.Success)
+    val app: Kleisli[IO, Request[IO], Response[IO]] = Router(
+      "/" -> drugRoutes
+    ).orNotFound
+    EmberServerBuilder
+      .default[IO]
+      .withHost(ipv4"0.0.0.0")
+      .withPort(port"8080")
+      .withHttpApp(app)
+      .build
+      .use((_: Server) => IO.never)
+      .as(ExitCode.Success)
   }
 
-  def drugRoutes: HttpRoutes[IO] = {
-    val dsl = Http4sDsl[IO]
+  /** Returns HttpRoutes[IO] composed of a root/scrape/{url} route that takes an encoded url string and returns a
+    * sequence of website records serialised as a json (list of objects)
+    * @return HttpRoutes[IO] takes requests and outputs the response
+    */
+  private def drugRoutes: HttpRoutes[IO] = {
+    val dsl: Http4sDsl[IO] = Http4sDsl[IO]
     import dsl._
     HttpRoutes.of[IO] {
       case GET -> Root / "scrape" / encodedUrl =>
-        val drugResults = {
-          val url = URLDecoder.decode(encodedUrl, "utf-8")
-          new DrugTestScraper(url).getAllDrugTests().asJson
+        val drugResults: Json = {
+          val url: String = URLDecoder.decode(encodedUrl, "utf-8")
+          new DrugTestScraper(url).getAllDrugTests.asJson
         }
         Ok(drugResults)
       case _ => NotFound("Invalid query")
     }
   }
 }
-
-
--- src/main/scala/drugscraper/DrugTestScraper.scala
+++ src/main/scala/drugscraper/DrugTestScraper.scala
@@ -4,121 +4,188 @@ import drugscraper.DrugTestScraper._
 import net.ruippeixotog.scalascraper
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
 import net.ruippeixotog.scalascraper.dsl.DSL._
-import net.ruippeixotog.scalascraper.scraper.ContentExtractors.{element, text, texts}
+import net.ruippeixotog.scalascraper.model.Element
+import net.ruippeixotog.scalascraper.scraper.ContentExtractors.{
+  element,
+  text,
+  texts
+}
 import org.jsoup.Connection
 
 import java.time.LocalDate
 import java.time.format.{DateTimeFormatter, DateTimeParseException}
 import java.util.concurrent.TimeUnit
 
-
 class DrugTestScraper(drugsPage: String) {
-  private val browser = new JsoupBrowser {
+  private val browser: JsoupBrowser = new JsoupBrowser {
     override def requestSettings(conn: Connection): Connection =
       conn.timeout(30 * 60000)
   }
-  private val tests = browser.get(drugsPage)
 
-  def getAllDrugTests(): Seq[WebsiteRecord] = {
-    getAllRowElements().map(el => {TimeUnit.SECONDS.sleep(1); getWebsiteRecord(el)}).toSeq
+  private val tests: JsoupBrowser.JsoupDocument = browser.get(drugsPage)
+
+  /** Returns all drug tests that are present in the url drugsPage
+    * @return Seq[WebsiteRecord]
+    */
+  def getAllDrugTests: Seq[WebsiteRecord] = {
+    getAllRowElements
+      .map((el: Element) => {
+        TimeUnit.SECONDS.sleep(1)
+        getWebsiteRecord(el)
+      })
+      .toSeq
   }
 
-  def getAllRowElements(): Iterable[scalascraper.model.Element] =
+  /** Returns all row elements that contain the detailed drug test URL to be scraped
+    * @return
+    */
+  private def getAllRowElements: Iterable[scalascraper.model.Element] =
     (tests >> element("#MainResults") >> element("tbody")).children
 
-  def getWebsiteRecord(rowElement: scalascraper.model.Element) = {
-    val sampleNameElement = (rowElement >> element(".sample-name"))
+  /** Access the row element, then gets the detailed drug page HTML and parses the drug data to a WebsiteRecord
+    * @param rowElement
+    * @return
+    */
+  private def getWebsiteRecord(
+      rowElement: scalascraper.model.Element
+  ): WebsiteRecord = {
+    val sampleNameElement: Element = rowElement >> element(".sample-name")
 
-    val singleDrugTestPage = browser.get(getDrugTestUrl(sampleNameElement))
+    val singleDrugTestPage: JsoupBrowser.JsoupDocument =
+      browser.get(getDrugTestUrl(sampleNameElement))
 
-    val detailsModule = singleDrugTestPage >> element(".DetailsModule")
+    val detailsModule: Element = singleDrugTestPage >> element(".DetailsModule")
 
-    val rightTbody =
-      detailsModule >> element(
-        ".TabletDataRight"
-      ) >> element("tbody")
+    val rightTbody: Element =
+      detailsModule >> element(".TabletDataRight") >> element("tbody")
 
-    val leftTbody = detailsModule >> element(
-      ".TabletDataLeft"
-    ) >> element("tbody")
+    val leftTbody: Element =
+      detailsModule >> element(".TabletDataLeft") >> element("tbody")
 
-    val id = leftTbody.select(":eq(1)").head.text.toIntOption
+    val id: Option[Int] = leftTbody.select(":eq(1)").head.text.toIntOption
 
-    val pictureUrl = (detailsModule >?> element(".TabletPhoto-set") >?>
-      element(".TabletMedium")).flatten.map(el => el.attr("src"))
+    val pictureUrl: Option[String] =
+      (detailsModule >?> element(".TabletPhoto-set") >?>
+        element(".TabletMedium")).flatten.map((el: Element) => el.attr("src"))
 
-    val soldAs =
-      (sampleNameElement >?> text(".sold-as")).flatMap(_.split(": ").lastOption)
+    val soldAs: Option[String] =
+      (sampleNameElement >?> text(".sold-as")).flatMap(
+        (_: String).split(": ").lastOption
+      )
 
-    val sampleName =
-      (sampleNameElement >?> text("a"))
+    val sampleName: Option[String] =
+      sampleNameElement >?> text("a")
 
-    val substances =
-      (rowElement >?> texts(".Substance li")).map(_.toList)
+    val substances: Option[List[String]] =
+      (rowElement >?> texts(".Substance li")).map((_: Iterable[String]).toList)
 
-    val amounts = {
-      val amounts = rowElement >?> texts(".Amounts li")
-      (for (strAmounts <- amounts) yield strAmounts.map(_.toDoubleOption).toList.flatten)
+    val amounts: Option[List[Double]] = {
+      val amounts: Option[Iterable[String]] =
+        rowElement >?> texts(".Amounts li")
+      for (strAmounts <- amounts)
+        yield strAmounts.map((_: String).toDoubleOption).toList.flatten
     }
 
-    val testDate = parseDrugTestDateStrToDate(rightTbody.select(":eq(1)")
-      .head.text)
+    val testDate: Option[String] = parseDrugTestDateStrToDate(
+      rightTbody.select(":eq(1)").head.text
+    )
 
-    val srcLocation = getDrugTestTbodyAttribute(rightTbody, 2)
+    val srcLocation: Option[String] = getDrugTestTbodyAttribute(rightTbody, 2)
 
-    val submitterLocation = getDrugTestTbodyAttribute(rightTbody, 3)
+    val submitterLocation: Option[String] =
+      getDrugTestTbodyAttribute(rightTbody, 3)
 
-    val colour = getDrugTestTbodyAttribute(rightTbody, 4)
+    val colour: Option[String] = getDrugTestTbodyAttribute(rightTbody, 4)
 
-    val size = getDrugTestTbodyAttribute(rightTbody, 5)
+    val size: Option[String] = getDrugTestTbodyAttribute(rightTbody, 5)
 
-    WebsiteRecord(id, soldAs, pictureUrl, sampleName, substances, amounts,
-      testDate, srcLocation, submitterLocation, colour, size)
+    WebsiteRecord(
+      id,
+      soldAs,
+      pictureUrl,
+      sampleName,
+      substances,
+      amounts,
+      testDate,
+      srcLocation,
+      submitterLocation,
+      colour,
+      size
+    )
 
   }
-
 
 }
 
 object DrugTestScraper {
-  private val drugsDataRootUrl = "https://www.drugsdata.org/"
+  private val drugsDataRootUrl: String = "https://www.drugsdata.org/"
 
+  /** Returns the URL for the detailed drug test page from a row element in the main table
+    * @param sampleNameElementInRow the component of the row containing the sample name
+    * @return drugTestUrl the url to the page containing more drug info
+    */
   private def getDrugTestUrl(
-                              sampleNameElementInRow: scalascraper.model
-                              .Element
-                            ): String =
+      sampleNameElementInRow: scalascraper.model.Element
+  ): String =
     drugsDataRootUrl + (sampleNameElementInRow >> element("a"))
       .attr("href")
 
+  /** Returns the item at index from the detailed drug table in the nested page
+    * @param tbody detailed drug table
+    * @param index index (including 0)
+    * @return itemString
+    */
   private def getDrugTestTbodyAttribute(
-                                         tbody: scalascraper.model.Element,
-                                         index: Int
-                                       ): Option[String] = {
+      tbody: scalascraper.model.Element,
+      index: Int
+  ): Option[String] = {
     try {
       Some(tbody.select(s":eq($index)").head.children.tail.head.text)
     } catch {
-      case e: NoSuchElementException => None
+      case _: NoSuchElementException => None
     }
   }
 
+  /** Returns an ISO formatted string from a string formatted MMM dd, uuuu
+    * @param scrapedStr date scraped from table
+    * @return Option[DateString] ISO formatted Option[String]
+    */
   private def parseDrugTestDateStrToDate(scrapedStr: String): Option[String] = {
-    val formatter = DateTimeFormatter.ofPattern("MMM dd, uuuu")
+    val formatter: DateTimeFormatter =
+      DateTimeFormatter.ofPattern("MMM dd, uuuu")
     try {
-      val localDate = LocalDate.parse(scrapedStr, formatter).toString
+      val localDate: String = LocalDate.parse(scrapedStr, formatter).toString
       Some(localDate)
     } catch {
-      case e: DateTimeParseException => None
+      case _: DateTimeParseException => None
     }
   }
 
-  case class WebsiteRecord(id: Option[Int], soldAs: Option[String],
-                           pictureUrl: Option[String],
-                           sampleName: Option[String],
-                           substances: Option[List[String]],
-                           amounts: Option[List[Double]],
-                           testDate: Option[String],
-                           srcLocation: Option[String],
-                           submitterLocation: Option[String],
-                           colour: Option[String], size: Option[String])
+  /** Schema for drug tests to be scraped
+    * @param id unique id
+    * @param soldAs name submitted by the sender
+    * @param pictureUrl href for the photo of the drug
+    * @param sampleName name created by the tester
+    * @param substances list of chemical substances in order of amount
+    * @param amounts list of amounts
+    * @param testDate date of test
+    * @param srcLocation location string, usually of format City, State or Country
+    * @param submitterLocation location string, usually of format City, State or Country
+    * @param colour colour
+    * @param size size in variable units
+    */
+  case class WebsiteRecord(
+      id: Option[Int],
+      soldAs: Option[String],
+      pictureUrl: Option[String],
+      sampleName: Option[String],
+      substances: Option[List[String]],
+      amounts: Option[List[Double]],
+      testDate: Option[String],
+      srcLocation: Option[String],
+      submitterLocation: Option[String],
+      colour: Option[String],
+      size: Option[String]
+  )
 
 }
