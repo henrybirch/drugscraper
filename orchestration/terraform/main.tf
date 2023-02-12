14594d7c6e395ec99ffe1ab885b6f1438bd62a0d
henrybirch <hab.b@hotmail.co.uk>
2023-02-12 13:44:24 +0000

    remove cloud run skeleton

--- orchestration/terraform/main.tf
+++ orchestration/terraform/main.tf
@@ -27,28 +27,7 @@ resource "google_artifact_registry_repository" "artifact_repo" {
   format        = "DOCKER"
 }
 
-resource "google_cloud_run_service" "example_service" {
-  name     = "example-service"
-  location = "<REGION>"
 
-  template {
-    spec {
-      containers {
-        image = "gcr.io/${var.gcp_project}/${var.artifact_repository}/${var.drugscraper_api_repository}:${var.drugscraper_api_image_tag}"
-        env   = [
-          {
-            name  = "VAR_NAME_1"
-            value = "value1"
-          },
-          {
-            name  = "VAR_NAME_2"
-            value = "value2"
-          }
-        ]
-      }
-    }
-  }
-}
 
 
 
