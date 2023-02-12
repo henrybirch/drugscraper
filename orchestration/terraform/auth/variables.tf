44de57ec3fdb34f58dbde7ebf3323143a7777d65
henrybirch <hab.b@hotmail.co.uk>
2023-02-12 13:43:13 +0000

    extract all auth vars into separate file

---
+++ orchestration/terraform/auth/variables.tf
@@ -0,0 +1,17 @@
+variable "gcp_project" {
+  type    = string
+  default = "drugscraper-377523"
+}
+
+variable "sa_id" {
+  type    = string
+  default = "drugscraper-sa"
+}
+
+variable "github_owner" {
+  type = string
+}
+
+variable "drugscraper_repository_name" {
+  type = string
+}
