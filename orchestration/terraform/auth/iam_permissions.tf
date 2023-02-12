c06ddeda695badb0518172c68c0db9dbdc1b47f1
henrybirch <hab.b@hotmail.co.uk>
2023-02-12 13:42:18 +0000

    extract iam permissions into separate file

---
+++ orchestration/terraform/auth/iam_permissions.tf
@@ -0,0 +1,11 @@
+resource "google_project_iam_member" "gcs_admin" {
+  member  = "serviceAccount:${google_service_account.sa.email}"
+  project = var.gcp_project
+  role    = "roles/storage.admin"
+}
+
+resource "google_service_account_iam_member" "wif_sa" {
+  service_account_id = "projects/${var.gcp_project}/serviceAccounts/${google_service_account.sa.email}"
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.cicd_pool.name}/attribute.repository/${var.github_owner}/${var.drugscraper_repository_name}"
+}
