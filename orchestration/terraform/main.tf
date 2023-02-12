3211fa416056c1f8b4665c0b76ec65ddc57d9224
henrybirch <hab.b@hotmail.co.uk>
2023-02-12 17:33:50 +0000

    add submodules to main

--- orchestration/terraform/main.tf
+++ orchestration/terraform/main.tf
@@ -20,11 +20,12 @@ provider "github" {
   owner = var.github_owner
 }
 
-resource "google_artifact_registry_repository" "artifact_repo" {
-  location      = var.default_location
-  repository_id = var.artifact_repository
-  description   = "Repo for drugscraper api and get-all images"
-  format        = "DOCKER"
+module "auth" {
+  source = "./auth"
+}
+
+module "artifact_repository" {
+  source = "./artifact_repository"
 }
 
 
