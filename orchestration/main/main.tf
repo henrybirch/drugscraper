bd7d38bbe8c198f82c7b9a2de79ed2dab6944600
henrybirch <hab.b@hotmail.co.uk>
2023-02-13 22:55:21 +0000

    move all of orchestration except artifact repository into a nested folder
    This is because the artifact repository needs to be made. Then the image needs to be uploaded. And then the rest of orchestration needs to occur

--- orchestration/main/main.tf
+++ orchestration/main/main.tf
@@ -24,14 +24,14 @@ provider "github" {
 
 
 module "artifact_repository" {
-  source                 = "./artifact_repository"
+  source                 = "../artifact_repository"
   artifact_repository_id = var.artifact_repository_id
   default_location       = var.default_location
 }
 
 module "scrape_api_cloud_run" {
   depends_on       = [module.artifact_repository]
-  source           = "./scrape_api_cloud_run"
+  source           = "scrape_api_cloud_run"
   api_name         = "drugscraper-api"
   default_location = var.default_location
   image_name       = var.drugscraper_api_image_name
@@ -39,7 +39,7 @@ module "scrape_api_cloud_run" {
 }
 
 module "inject_image_info_github" {
-  source                      = "./inject_image_info_github"
+  source                      = "inject_image_info_github"
   api_image_tag               = var.drugscraper_api_image_tag
   api_image_name              = var.drugscraper_api_image_name
   artifact_repository_url     = join("/", ["gcr.io", var.gcp_project, var.artifact_repository_id])
