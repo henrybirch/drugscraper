a2c55be7df34b574db64d072de9a8a41a00d2bb1
henrybirch <hab.b@hotmail.co.uk>
2023-02-13 19:24:05 +0000

    fix role creation for artifact registry admin for sa

--- gcp_auth_init/iam_permissions.tf
+++ gcp_auth_init/iam_permissions.tf
@@ -1,23 +1,23 @@
-resource "google_project_iam_member" "gcs_admin" {
-  member  = "serviceAccount:${google_service_account.sa.email}"
-  project = var.gcp_project
-  role    = "roles/storage.admin"
+resource "google_service_account_iam_member" "wif_sa" {
+  service_account_id = "projects/${var.gcp_project}/serviceAccounts/${google_service_account.sa.email}"
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.cicd_pool
+  .name}/attribute.repository/henrybirch/drugscraper"
 }
 
 locals {
-  member = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.cicd_pool
-  .name}/attribute.repository/henrybirch/drugscraper"
-  service_account_id = "projects/${var.gcp_project}/serviceAccounts/${google_service_account.sa.email}"
+  sa_member = "serviceAccount:${google_service_account.sa.email}"
 }
 
-resource "google_service_account_iam_member" "wif_sa" {
-  service_account_id = local.service_account_id
-  role               = "roles/iam.workloadIdentityUser"
-  member             = local.member
+resource "google_project_iam_member" "gcs_admin" {
+  member  = "serviceAccount:${google_service_account.sa.email}"
+  role    = "roles/storage.admin"
+  project = var.gcp_project
 }
 
-resource "google_service_account_iam_member" "artifact_repository" {
-  role   = "roles/artifactregistry.admin"
-  member = local.member
-  service_account_id = local.service_account_id
+
+resource "google_project_iam_member" "artifact_repository" {
+  role               = "roles/artifactregistry.admin"
+  member             = local.sa_member
+  project = var.gcp_project
 }
