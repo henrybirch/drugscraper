1acda83fa3b07eb8f5a5ef751ae45af0691f674b
henrybirch <hab.b@hotmail.co.uk>
2023-02-13 14:05:31 +0000

    remove unneeded var

--- gcp_auth_init/iam_permissions.tf
+++ gcp_auth_init/iam_permissions.tf
@@ -7,5 +7,6 @@ resource "google_project_iam_member" "gcs_admin" {
 resource "google_service_account_iam_member" "wif_sa" {
   service_account_id = "projects/${var.gcp_project}/serviceAccounts/${google_service_account.sa.email}"
   role               = "roles/iam.workloadIdentityUser"
-  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.cicd_pool.name}/attribute.repository/${var.drugscraper_repository}"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.cicd_pool
+  .name}/attribute.repository/${var.github_owner}/${var.drugscraper_repository_name}"
 }
